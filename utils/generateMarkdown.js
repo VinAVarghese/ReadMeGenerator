dc01b15d4313056d90d70d775ee6a614de98f4fd
Vins MacBook <vinavarghese@gmail.com>
2020-07-04 19:03:26 -0700

    generator function filled out with md formatting

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,28 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `# ${data.title} ${data.badge}
+  > ${data.description}
+  ### Table of Contents
+  1. Installation
+  2. Usage
+  3. License
+  4. Contributing
+  5. Tests
+  6. Questions
+  ### Installation
+  > ${data.installation}
+  ### Usage
+  > ${data.usage}
+  ### License
+  > ${data.license}
+  ### Contributing
+  > ${data.contributions}
+  ### Tests
+  > ${data.test}
+  ### Questions
+  [${gitHub}](https://github.com/${gitHub})
+  ${data.email}
+  > ${data.questions}
 `;
 }
 
